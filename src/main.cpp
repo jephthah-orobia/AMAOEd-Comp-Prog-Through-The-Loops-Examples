a94dbf98fcaf218381fc33580ac77d62a3594a89
jephthah-orobia <jephthah.orobia@gmail.com>
2025-04-16 15:09:09 -0500

    Task implemented!

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,37 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#define NOMINMAX
+#include <limits>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
+  int n = 10;
+
+  do {
+    system("clear");
+    if(cin.fail() || n < 0)
+      cout << "\033[31m<Input is invalid, please try again>\033[0m" << endl;
+    if(cin.fail()){
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    cout << "Start from (seconds or 0 to exit): ";
+    cin >> noskipws >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if(!cin.fail() && n > 0){
+      for(int i = n; i >= 0; i--){
+        system("clear");
+        cout << i;
+        Sleep(1000);
+      }
+    } else if(cin.fail()){
+      n = 10;
+    }
+  } while( n != 0 );
+
   return EXIT_SUCCESS;
 }
