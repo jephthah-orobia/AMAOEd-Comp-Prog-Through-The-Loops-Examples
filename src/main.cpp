d27abbd890d2453b5635b88b2806706dfc321fa4
jephthah-orobia <jephthah.orobia@gmail.com>
2025-04-16 15:53:29 -0500

    Implement the task!

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 /* ****************************************************************
- * @author: <your name here>
- * @app name: <your application name here>
- * @app desc: <describe the purpose of your application here>
- * @history:
- *  - <yyyy/mm/dd> | <name>
- *      -- <description>
- *      -- <description>
- *      -- <description>
- *  - <yyyy/mm/dd> | <name>
- *      -- <description>
- *      -- <description>
- *      -- <description>
+ * @author: Jephthah M. Orobia
+ * @app name: Names
+ * @app desc: Greeting names from the array.
  * ****************************************************************/
 
 #include <cstdio>
@@ -33,8 +24,10 @@ int main()
   // ************************** TO DO **************************
   // Place your code logic after this comment line
   // ***********************************************************
+  string names[50] = { "Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava", "William", "Sophia", "Elijah", "Isabella", "James", "Mia", "Benjamin", "Amelia", "Lucas", "Harper", "Mason", "Evelyn", "Ethan", "Abigail", "Alexander", "Ella", "Henry", "Lily", "Jacob", "Aria", "Michael", "Avery", "Daniel", "Sofia", "Logan", "Camila", "Jackson", "Scarlett", "Sebastian", "Victoria", "Jack", "Madison", "Aiden", "Luna", "Owen", "Grace", "Samuel", "Chloe", "Matthew", "Penelope", "Joseph", "Layla", "Levi", "Riley"};
 
-  cout << "Hello World!" << endl;
+  for(string name:names)
+    cout << "Hello "<< name <<"!" << endl;
 
   // ********************** DO NOT CHANGE **********************
   // Print a new line and ask user for any key before exiting
